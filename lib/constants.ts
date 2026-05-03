c54e78b450f9d9a017ec1d8dae07adfc5c6313fe
dayCod <alayarahman07@gmail.com>
2026-05-03 16:11:30 +0700

    feat: update whatsappNumber in SITE_CONFIG to correct contact information

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,7 +2,7 @@ export const SITE_CONFIG = {
   name: "Gathering Keluarga Surosentono 2025",
   tagline: "Mempererat Tali Silaturahmi",
   url: "https://surosentono-gathering.vercel.app",
-  whatsappNumber: "62812XXXXXXXX", // Ganti dengan nomor WA panitia
+  whatsappNumber: "6281296601429", // Ganti dengan nomor WA panitia
   googleFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLSediQ_lHAOCIEGeEXmghEDrlmTtNB5KmR8dopiaHfv5gmysAg/viewform", // Ganti dengan URL Google Form RSVP
 };
 
